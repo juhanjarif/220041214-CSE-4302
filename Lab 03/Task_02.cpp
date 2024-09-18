cdd363cc3b5ff01cb8b422e9e17cfcf36738bf78
Juhan Ahmed Jarif <ahmadjarif2004@gmail.com>
2024-09-18 23:16:49 +0600

    Create Task_02.cpp

---
+++ Lab 03/Task_02.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class RationalNumber{
+private:
+    int numerator = 0;
+    int denominator = 1;
+
+public:
+    void assign(int input_numerator, int input_denominator){
+        if (input_denominator == 0) {
+            cout << "You can not assign 0 as denominator." << endl;
+            exit(0);
+        }
+        numerator = input_numerator;
+        denominator = input_denominator;
+    }
+
+    double convert(){
+        return float (numerator)/(denominator);
+    }
+
+    void invert(){
+        if (numerator == 0) {
+            cout << "You can not assign 0 as denominator. Inversion Failed." << endl;
+            exit(0);
+        }
+        int temp = numerator;
+        numerator = denominator;
+        denominator = temp;
+    }
+
+    void print() {
+        cout << "The Rational Number is " << numerator << "/" << denominator << endl;
+    }
+};
+
+int main() {
+    RationalNumber rational;
+
+    rational.assign(-3, 1);
+    rational.print();
+
+    cout << "Decimal equivalent: " << rational.convert() << endl;
+
+    rational.invert();
+    rational.print();
+
+    rational.assign(5, 0);
+
+    return 0;
+}
